cb9d29dca24374199a56d15de664e8a4d92e60a2
Joseph <josephgreat2003@gmail.com>
2023-08-26 19:36:38 +0100

    mobile landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,20 +1,20 @@
 import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
-import { GiBottomRight3DArrow } from "react-icons/gi";
+import { HiArrowRight } from "react-icons/hi";
 
 export default function LandingPage() {
   return (
     <Flex h="100vh" bg="url(/images/bg/mobilelanding_bg.jpg) no-repeat" bgSize={"cover"}>
-      <Container mt="auto">
+      <Container mt="auto" mb="2rem">
         <Heading as="h1" fontSize={"3rem"}>
           QuickEat
         </Heading>
-        <Text>
+        <Text fontWeight={"400"} my="1">
           offering you the best nutrition for your diet, fast and reliant.
         </Text>
-        <Flex>
-            <GiBottomRight3DArrow />
-            <Text>Get <br/>Started</Text>
+        <Flex alignItems={"center"} gap="2">
+            <HiArrowRight size={"2rem"} />
+            <Text fontWeight={"500"}>Get <br/>Started</Text>
         </Flex>
       </Container>
     </Flex>
