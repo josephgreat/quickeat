cab0dd7e365b14a393de3e6a8f70aa6649224ac3
Joseph <josephgreat2003@gmail.com>
2023-08-26 19:41:17 +0100

    made changes in mobile landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,14 +5,14 @@ import { HiArrowRight } from "react-icons/hi";
 export default function LandingPage() {
   return (
     <Flex h="100vh" bg="url(/images/bg/mobilelanding_bg.jpg) no-repeat" bgSize={"cover"}>
-      <Container mt="auto" mb="2rem">
+      <Container mt="auto">
         <Heading as="h1" fontSize={"3rem"}>
           QuickEat
         </Heading>
-        <Text fontWeight={"400"} my="1">
+        <Text fontWeight={"600"} fontSize={".8rem"} fontFamily={"'Open Sans', sans-serif"}>
           offering you the best nutrition for your diet, fast and reliant.
         </Text>
-        <Flex alignItems={"center"} gap="2">
+        <Flex alignItems={"center"} fontWeight={"600"} gap="2" my="8">
             <HiArrowRight size={"2rem"} />
             <Text fontWeight={"500"}>Get <br/>Started</Text>
         </Flex>
